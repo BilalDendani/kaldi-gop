38ee1a51f2f756a38fb90f6327812ce69de02c07
Guanlong Zhao <gzhao@tamu.edu>
2018-02-22 18:32:12 -0600

    setup sample data path

--- egs/gop-compute/run_tri6b.sh
+++ egs/gop-compute/run_tri6b.sh
@@ -22,7 +22,7 @@ fi
 
 decode_nj=3
 
-data_dir=~/Desktop/learn_kaldi/kaldi/egs/anyone/data/aba
+data_dir=data/aba
 
 # Decode
 local/compute-gmm-gop.sh --nj "$decode_nj" --cmd "$decode_cmd" "$data_dir" data/lang_librispeech exp/tri6b exp/eval_gop_tri6b_aba
